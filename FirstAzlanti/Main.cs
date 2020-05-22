91f50de971c7714b19d79c65b61aa2c7feb44e6b
Lucian Poston <lucianposton@pm.me>
2020-05-21 22:16:38 -0500

    PenultimateAzlanti

--- FirstAzlanti/Main.cs
+++ FirstAzlanti/Main.cs
@@ -3,6 +3,8 @@ using Kingmaker.Controllers;
 using Kingmaker.EntitySystem.Persistence;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,71 +12,110 @@ using UnityModManagerNet;
 using Newtonsoft.Json;
 using Kingmaker.UI.SettingsUI;
 
-namespace FirstAzlanti
+namespace PenultimateAzlanti
 {
-    public static class Settings
-    {
-        [JsonProperty]
-        public static bool EnableKeepAzlanti = true;
-        [JsonProperty]
-        public static bool BackupAzlantiOnAutoSave = true;
-    }
-
     public static class Main
     {
-        public static UnityModManager.ModEntry.ModLogger Logger;
+        public class Settings
+        {
+            public Settings()
+            {
+                EnableKeepAzlanti = true;
+                BackupPreviousAzlantiOnAutoSave = true;
+                BackupAzlantiOnAutoSave = true;
+            }
+
+            public bool EnableKeepAzlanti { get; set; }
+            public bool BackupPreviousAzlantiOnAutoSave { get; set; }
+            public bool BackupAzlantiOnAutoSave { get; set; }
+        }
+
+        public static UnityModManager.ModEntry.ModLogger logger;
         private static Harmony12.HarmonyInstance harmony;
+        private static Settings settings = new Settings();
 
         internal static bool Load(UnityModManager.ModEntry modEntry)
         {
-            Logger = modEntry.Logger;
+            logger = modEntry.Logger;
 
             harmony = Harmony12.HarmonyInstance.Create(modEntry.Info.Id);
             harmony.PatchAll(typeof(Main).Assembly);
 
-            return true;
-        }
-    }
-
-    [Harmony12.HarmonyPatch(typeof(GameOverIronmanController), nameof(GameOverIronmanController.Activate))]
-    public static class KeepSavePatch
-    {
-        private static bool Prefix()
-        {
-            if (Settings.EnableKeepAzlanti)
-            {
-                Main.Logger.Log("Prevented Iron Man loss.");
-                LoadingProcess.Instance.ResetManualLoadingScreen();
-                return false;
+            try {
+                using (StreamReader file = File.OpenText(UnityModManager.modsPath + @"/PenultimateAzlanti/settings.json"))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    settings = (Settings)serializer.Deserialize(file, typeof(Settings));
+                }
             }
+            catch (Exception e)
+            {
+                logger.Log($"Failed to load custom settings.json due to {e.GetType().ToString()}. Using defaults.");
+            }
+
             return true;
         }
-    }
 
-    [Harmony12.HarmonyPatch(typeof(SaveManager), nameof(SaveManager.SaveRoutine))]
-    public static class BackupSavePatch
-    {
-        private static void Prefix(SaveInfo saveInfo)
+        [Harmony12.HarmonyPatch(typeof(GameOverIronmanController), nameof(GameOverIronmanController.Activate))]
+        public static class KeepSavePatch
         {
-            //Main.Logger.Log("Saving game... " + (saveInfo.Type == SaveInfo.SaveType.IronMan).ToString() + ":" + SettingsRoot.Instance.OnlyOneSave.CurrentValue.ToString());
-            if (Settings.BackupAzlantiOnAutoSave && (saveInfo.Type == SaveInfo.SaveType.IronMan || SettingsRoot.Instance.OnlyOneSave.CurrentValue))
+            private static bool Prefix()
             {
-                
-                string copy = saveInfo.FolderName.Substring(0, saveInfo.FolderName.Length-4);
-                try
+                if (settings.EnableKeepAzlanti)
                 {
-
-                    System.IO.File.Copy(saveInfo.FolderName, copy, true);
-                    Main.Logger.Log("Backuped Iron Man save: " + copy);
+                    logger.Log("Prevented Iron Man loss.");
+                    LoadingProcess.Instance.ResetManualLoadingScreen();
+                    return false;
                 }
-                catch (Exception)
-                {
-                    Main.Logger.Log("Save backup failed.");
-                }
+                return true;
             }
         }
 
+        [Harmony12.HarmonyPatch(typeof(SaveManager), nameof(SaveManager.SaveRoutine))]
+        public static class BackupPreviousSavePatch
+        {
+            private static void Prefix(SaveInfo saveInfo)
+            {
+                //Main.Logger.Log("Saving game... " + (saveInfo.Type == SaveInfo.SaveType.IronMan).ToString() + ":" + SettingsRoot.Instance.OnlyOneSave.CurrentValue.ToString());
+                if (settings.BackupPreviousAzlantiOnAutoSave && (saveInfo.Type == SaveInfo.SaveType.IronMan || SettingsRoot.Instance.OnlyOneSave.CurrentValue))
+                {
+                    string copy = saveInfo.FolderName + ".previous.bak";
+                    try
+                    {
+                        System.IO.File.Copy(saveInfo.FolderName, copy, true);
+                        logger.Log("Backup of previous Iron Man savegame created: " + copy);
+                    }
+                    catch (Exception e)
+                    {
+                        logger.Log("Backup of previous Iron Man savegame failed: " + e.ToString());
+                    }
+                }
+            }
+        }
+
+        [Harmony12.HarmonyPatch]
+        public static class BackupSavePatch
+        {
+            static System.Reflection.MethodBase TargetMethod() {
+                return typeof(SaveManager).GetMethod("SerializeAndSaveThread", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            }
+
+            private static void Postfix(SaveInfo saveInfo)
+            {
+                if (settings.BackupAzlantiOnAutoSave && (saveInfo.Type == SaveInfo.SaveType.IronMan || SettingsRoot.Instance.OnlyOneSave.CurrentValue))
+                {
+                    string copy = saveInfo.FolderName + ".bak";
+                    try
+                    {
+                        System.IO.File.Copy(saveInfo.FolderName, copy, true);
+                        logger.Log("Backup of Iron Man savegame created: " + copy);
+                    }
+                    catch (Exception e)
+                    {
+                        logger.Log("Backup of Iron Man savegame failed: " + e.ToString());
+                    }
+                }
+            }
+        }
     }
-
-
 }
--- FirstAzlanti/Properties/AssemblyInfo.cs
+++ FirstAzlanti/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("FirstAzlanti")]
+[assembly: AssemblyTitle("PenultimateAzlanti")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FirstAzlanti")]
+[assembly: AssemblyProduct("PenultimateAzlanti")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
-[assembly: Guid("947db780-515a-4ff5-ba7b-6032c7038384")]
+[assembly: Guid("947db780-515a-405f-ba7b-6032c7038384")]
 
 // Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
 //
